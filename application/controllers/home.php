4b231763d609652fe4f5607a38ae87e409dfe8d0
widret <m..widret.ts@gmail.com>
2013-10-14 09:15:17 +0900

    testcode

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -11,15 +11,12 @@ class Home extends MY_Controller {
 		$this -> load -> model('Event','event');
 		$user_id = $this->session->set_userdata('user_id');
 		
- 		$data = "";
- 		
  		$array = (1,2,3,4,5);
 		print_r($array);
 		var_dump($array);
  		
  		$myprofile = $this -> user -> getUser(0);
  		var_dump($myprofile);
- 		
  		
  		
  		/*
@@ -35,13 +32,11 @@ class Home extends MY_Controller {
  			$data['event_' + $i + '_walk'] = $this -> event -> get_event_walk($event_id);
  			$i++;
  		}
- 		*/
  		
  		$data['page_title'] = '卓飲み.com トップページ';
  		$data['user_img'] = $this -> user -> get_User(user_id);
  		$data['user_name'] = $this -> user -> get_User(user_id);
  		
- 		/*
  		$taglist = $this -> user -> get_top_tags();
  		$i = 0;
  		foreach($taglist as $tag){
@@ -49,11 +44,9 @@ class Home extends MY_Controller {
  			$i++;
  		}
  		*/
-		
-		var_dump($data);
-		
-		$this->load->view('header.php',$data);
-		$this->load->view('home/main.php',$data);
-		$this->load->view('footer.php',$data);
+ 		
+		//$this->load->view('header.php',$data);
+		//$this->load->view('home/main.php',$data);
+		//$this->load->view('footer.php',$data);
 	}
 }
